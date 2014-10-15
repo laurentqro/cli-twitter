d5f67126e1baf3b4b77791b089d07ba8c97cc365
Laurent Curau <laurentcurau@gmail.com>
2014-10-15 12:24:07 +0100

    Test for presence of a timestamp

--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -4,10 +4,6 @@ describe Message do
   let(:bob) { User.new('Bob') }
   subject(:message) { Message.new(:text => 'Good game though.', :user => :bob) }
 
-  it "belongs to a user" do
-    expect(subject.user).to eql :bob
-  end
-
   it "has a timestamp" do
     expect(subject.publication_date).to be_kind_of(Time)
   end
