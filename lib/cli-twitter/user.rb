003cca69b042afd08181856a9e59176a61170dbe
Laurent Curau <laurentcurau@gmail.com>
2014-10-24 16:51:41 +0100

    Refactor. Remove timestamps for now.

--- lib/cli-twitter/user.rb
+++ lib/cli-twitter/user.rb
@@ -2,15 +2,22 @@ module TwitterCli
   class User
     attr_accessor :name, :messages, :followers, :following
 
+    @@users = []
+
     def initialize(name)
       @name = name
       @messages = []
       @followers = []
       @following = []
+      @@users << self
+    end
+
+    def self.users
+      @@users
     end
 
     def publish(text)
-      message = Message.new({ text: text, author: self.name })
+      message = TwitterCli::Message.new({ text: text, author: self.name })
       @messages << message
       message
     end
@@ -21,34 +28,32 @@ module TwitterCli
     end
 
     def timeline
-      messages.sort_by(&:publication_date).reverse!
-      messages.map do |message|
-        "#{message.text} (#{message.publication_date})"
-      end
-      .join("\n")
+      timeline_messages = sort_by_publication_date(messages)
+      format_timeline_output(timeline_messages)
     end
 
     def wall
       wall_messages = messages + get_followed_users_messages
-      sort_by_publication_date(wall_messages)
+      wall_messages = sort_by_publication_date(wall_messages)
       format_wall_output(wall_messages)
     end
 
     private
 
     def get_followed_users_messages
-      following.map(&:messages)
+      following.any? ? following.map(&:messages).flatten : []
     end
 
     def sort_by_publication_date(messages)
-      messages.flatten!.sort_by(&:publication_date).reverse!
+      messages.sort_by(&:publication_date).reverse!
     end
 
     def format_wall_output(messages)
-      messages.map do |message|
-        "#{message.author} - #{message.text} (#{message.publication_date})"
-      end
-      .join("\n")
+      messages.map { |message| "#{message.author} - #{message.text}" }.join("\n")
+    end
+
+    def format_timeline_output(messages)
+      messages.map { |message| "#{message.text}" }.join("\n")
     end
   end
 end
